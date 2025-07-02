5f50c38d388f6ede181891826ef2ff81b6b308ad
Priyajit Mansingh <priyajitmansingh3@gmail.com>
2025-07-02 16:19:27 +0530

    mongodb has to be connect

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,28 @@
 import express from "express"
 import ImageKit from "imagekit";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// dotenv.config();
 
 const port = process.env.PORT || 3000
 const app=express()
 
-app.use(
-  cors({
-  origin:process.env.CLIENT_URL,
-})
-);
+// app.use(
+//   cors({
+//   origin:process.env.CLIENT_URL,
+// })
+// );
+
+// const connect=async () => {
+//   try{
+//     await mongoose.connect(process.env.MONGO)
+//     console.log("Connected to MongoDB")
+//   }catch(err){
+//     console.log(err)
+//   }
+// }
 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT, 
@@ -24,5 +37,6 @@ app.get('/api/upload', (req, res) => {
 
 
 app.listen(port, () => {
+  // connect()
   console.log(`Server is running on port ${port}`);
 })
